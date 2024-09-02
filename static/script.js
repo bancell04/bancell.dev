4a11b80708b1c7931a8e906c71346976790c2064
bancell04 <bancell@mines.edu>
2024-09-02 10:04:09 -0600

    added static files

---
+++ static/script.js
@@ -0,0 +1,15 @@
+function toggleCursor() {
+    const image = document.getElementById('cursor');
+    let isVisible = true;
+
+    setInterval(() => {
+        if (isVisible) {
+            image.style.visibility = 'hidden'; // or use image.classList.add('hidden');
+        } else {
+            image.style.visibility = 'visible'; // or use image.classList.remove('hidden');
+        }
+        isVisible = !isVisible;
+    }, 600);
+}
+
+toggleCursor();
